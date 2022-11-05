6672d0c8fce611b7bb439515ab8f2f92624d5b33
DaniilMih <115589963+DaniilMih@users.noreply.github.com>
2022-11-05 14:30:03 +0300

    Лабораторная работа 5

---
+++ Лабораторные 5/task5.py
@@ -0,0 +1,9 @@
+import random
+import string
+
+
+def get_random_password() -> str:
+    random_password = random.sample(string.hexdigits, 8)  # TODO написать функцию генерации случайных паролей
+    return random_password
+
+print(get_random_password())
